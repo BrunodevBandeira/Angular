7e9e8025ac3bf23af211517b1ec508dcea555c7e
Bruno Bandeira <brunodev29ab@gmail.com>
2021-06-22 21:02:35 -0300

    Inicio dos estudos em angular

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({  //é como se fosse uma anotação em java
+    selector: 'meu-primeiro-component',
+    template: `
+        <p> Meu primeiro component com angular </p>
+    `
+}) 
+
+export class MeuPrimeiroComponent {
+
+
+
+}
