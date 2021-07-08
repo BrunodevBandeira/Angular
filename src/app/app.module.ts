c5e6b6661c3cdd94a3522dc6793b992ab564cf1f
Bruno Bandeira <brunodev29ab@gmail.com>
2021-07-08 15:29:39 -0300

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+//OBS: a maioria dos decorators fazem parte do angular core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
+  //declarations: colocamos components, diretivas e pipes.
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
+  //imports: colocamos outros modulos que não sejam, components, diretivas e pipes.
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
+  //Providens é onde fica declarado todos os serviços disponiveis para todos os components declarados no modulo AppComponent(rotas ficam em providers)
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } //Os módulos de funcionalidades ficam desponiveis para toda a aplicação
--- src/app/meu-primeiro/meu-primeiro.component.ts
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,14 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from "@angular/core";//Os módulos são feitos em JS
 
-@Component({  //é como se fosse uma anotação em java
+@Component({  //@Component(decorator) é como se fosse uma anotação em java( tbm faz parte do JS)
     selector: 'meu-primeiro-component',
     template: `
         <p> Meu primeiro component com angular </p>
     `
-}) 
+})
 
-export class MeuPrimeiroComponent {
+export class MeuPrimeiroComponent { //Declarar a  classe desta forma é igual ao JS
 
 
 
 }
+
+
+//selector
+
